aa0e031fb3f3513cb4613fa474a10b2b89c3b194
Igor Aleshin <aleshin_igor@bah.com>
2020-10-06 16:36:03 -0400

    Added to git

---
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+  @Input() eventNum: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClickStartGame() {
+    console.log('Clicked onClickStartGame');
+  }
+}
