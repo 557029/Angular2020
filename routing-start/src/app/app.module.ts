46885eceb27fbb1818397973336c8b76bd3b1ce3
Igor Aleshin <aleshin_igor@bah.com>
2020-10-26 16:01:32 -0400

    Added Observable Route

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { RouterModule, Routes} from '@angular/router';
 const appRouter: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent },
-  {path: 'user/:id', component: UserComponent },
+  {path: 'users/:id/:name', component: UserComponent },
   {path: 'servers', component: ServersComponent }
 ];
 
