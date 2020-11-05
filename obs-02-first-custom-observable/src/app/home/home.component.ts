83f0a85766eb4a73073dd8bcf64b83faa0a5ac7f
Igor Aleshin <aleshin_igor@bah.com>
2020-11-05 14:21:22 -0500

    Added rest of obs projects

---
+++ obs-02-first-custom-observable/src/app/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import {interval, Observable, Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const customObservable = Observable.create( observer => {
+        let count = 0;
+        setInterval(() => {
+          observer.next(count);
+          count++;
+        }, 1000);
+      }
+    );
+    this.firstObsSubscription = customObservable.subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+}
