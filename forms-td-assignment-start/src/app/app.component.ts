3a1d98b5f89e4bce1184ced8171a9510ea1e6903
Igor Aleshin <aleshin_igor@bah.com>
2020-11-10 11:02:59 -0500

    Ended Forms-Td-Start

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') singUpForm;
+  subscriptionTypes = [
+    'Basic',
+    'Advanced',
+    'Pro'];
 
+  subscription = {
+    email: '',
+    password: '',
+    subscription: 'advanced'
+  };
+  defaultSubscription = this.subscriptionTypes[1];
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.singUpForm);
+    // this.subscription.email = this.singUpForm.value.
+    this.subscription.email = this.singUpForm.value.addressData.email;
+    this.subscription.password = this.singUpForm.value.addressData.password;
+    this.subscription.subscription = this.singUpForm.value.addressData.subscription;
+
+    console.log(this.subscription);
+    this.singUpForm.reset();
+  }
 }
