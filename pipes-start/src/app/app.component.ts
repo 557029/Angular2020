dc0afe930b6b3e9680a52d1db762572e0cbb996e
Igor Aleshin <aleshin_igor@bah.com>
2020-11-16 14:00:48 -0500

    Completed pipes chapter.

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, rejects) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   filteredStatus = '';
   servers = [
     {
@@ -39,5 +45,14 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(2020, 11, 12)
+    });
   }
 }
