7646910ca18a988a1fac5a0f1ef4edb88326ee3d
Igor Aleshin <aleshin_igor@bah.com>
2020-11-23 15:41:24 -0500

    Added lazy load feature for Recipes.

--- auth-01-auth-component/src/app/app-routing.module.ts
+++ auth-01-auth-component/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+  { path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full' },
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) }
 ];
 
 @NgModule({
